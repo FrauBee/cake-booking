f2ee99934a4be9f3a8b12031bd6f39e28f0d3171
FrauBee <s.borgwardt@gso.schule.koeln>
2026-03-18 10:51:43 +0100

    Update login.js

--- api/admin/login.js
+++ api/admin/login.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     }
 
     const { passwordHash } = req.body;
-    const expectedHash = '95017ef92c621c2474a2d93d53c18d8e24a60e2aac993ccc5030a5dbd168c4ba';
+    const expectedHash = '4885d164710a85a004e2187881233a7aa4459170209dd9938759e70aa9dd0c0d';
 
     if (passwordHash === expectedHash) {
         return res.status(200).json({ success: true });
